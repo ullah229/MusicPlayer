2b678e7978f533ea65a36fcc7e29024186a3d376
Neman Ullah <47436847+ullah229@users.noreply.github.com>
2020-07-26 13:38:18 +0200

    Add files via upload

---
+++ Player/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(19, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(134, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.PlayFile);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(16, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Duration";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(491, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "CurrentPos";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Location = new System.Drawing.Point(215, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 49);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(609, 160);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(461, 229);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 49);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.VolumeDown);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(542, 229);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 49);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.VolumeUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(525, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "0";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button6.Location = new System.Drawing.Point(296, 229);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 49);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Skip";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.skipbutton);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(24, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Songname";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(649, 331);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Music Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Player/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using System.IO;
+using System.Drawing.Text;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
+namespace Player
+{
+    public partial class Form1 : Form
+    {
+        private WMPLib.WindowsMediaPlayer wplayer = null;    
+        private string[] files;    
+        private bool musplay = false;
+        private Image MyImage;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 850;
+            timer2.Interval = 850;
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+              OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Multiselect = true;
+                ofd.Filter = "(mp3,wav,mp4,mov,wmv,mpg)|*.mp3;*.wav;*.mp4;*.mov;*.wmv;*.mpg|all files|*.*";
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    files = ofd.FileNames;
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        listBox1.Items.Add(files[i]);
+                    }
+                }
+        }
+        private void PlayFile(object sender, EventArgs e)
+        {
+            try
+            {
+                if (wplayer == null)
+                {
+                    wplayer = new WMPLib.WindowsMediaPlayer();
+                    wplayer.URL = listBox1.Text;
+                    wplayer.PlayStateChange += new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(wplayer_PlayStateChange);
+                }
+                musplay = !musplay;
+                if (musplay)
+                {
+                    button2.Text = "Playing";
+                    wplayer.controls.play();
+                    wplayer.settings.volume = 5;
+                    label4.Text = wplayer.currentMedia.getItemInfo("Title");
+                    label2.Text = wplayer.currentMedia.durationString;
+                    timer1.Start();
+                }
+                else
+                {
+                    wplayer.controls.pause();
+                    button2.Text = "Paused";
+                    timer1.Stop();
+                }              
+            }
+            catch
+            {
+                label4.Text = "Invalid action";
+            }
+        }
+
+        void wplayer_PlayStateChange(int NewState)
+        {
+            try
+            {
+                if (wplayer.playState == WMPLib.WMPPlayState.wmppsMediaEnded)
+                {
+                    listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+                    wplayer.URL = listBox1.Text;
+                    label2.Text = wplayer.currentMedia.durationString;
+                    label4.Text = wplayer.currentMedia.getItemInfo("Title");
+                    timer2.Enabled = true;
+                }
+            }
+            catch
+            {
+                if (wplayer.playState == WMPLib.WMPPlayState.wmppsMediaEnded)
+                {
+                    listBox1.SelectedIndex = -1;
+                    listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+                    timer2.Enabled = true;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                wplayer.controls.stop();
+                wplayer = null;
+                label2.Text = "0";
+                label3.Text = "0";
+            }
+            catch
+            {
+                label4.Text = "Invalid action";
+            }
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (wplayer == null)
+            {
+                wplayer = new WMPLib.WindowsMediaPlayer();     
+                wplayer.URL = listBox1.Text;
+                label5.Text = wplayer.settings.volume.ToString();
+                wplayer.PlayStateChange += new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(wplayer_PlayStateChange);
+            }
+            musplay = !musplay;
+            if (musplay || listBox1.SelectedItem != listBox1.SelectedItem)
+            {
+                button2.Text = "Playing";
+                wplayer.controls.play();
+                wplayer.settings.volume = 5;
+                label4.Text = wplayer.currentMedia.getItemInfo("Title");
+                label2.Text = wplayer.currentMedia.durationString;
+                timer1.Start();
+            }
+            else
+            {
+                button2.Text = "Paused";
+                wplayer.controls.stop();
+                wplayer = null;
+                timer1.Stop();
+            }       
+        }       
+
+        private void VolumeUp(object sender, EventArgs e)
+        {
+            try
+            {
+                if (wplayer.settings.volume < 100)
+                {
+                    wplayer.settings.volume = wplayer.settings.volume + 5;
+                    label5.Text = wplayer.settings.volume.ToString();
+                }
+            }
+            catch
+            {
+                label4.Text = "No song volume to adjust";
+            }
+        }
+
+        private void VolumeDown(object sender, EventArgs e)
+        {
+            try
+            {
+                if (wplayer.settings.volume >= 0)
+                {
+                    wplayer.settings.volume = wplayer.settings.volume - 5;
+                    label5.Text = wplayer.settings.volume.ToString();
+                }
+            }
+            catch
+            {
+                label4.Text = "No song volume to adjust";
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            try
+            {
+                label3.Text = wplayer.controls.currentPositionString;
+                label2.Text = wplayer.currentMedia.durationString;
+            }
+            catch
+            {
+                label4.Text = "Error";
+            }
+        }
+
+        private void skipbutton(object sender, EventArgs e)
+        {
+            try
+            { 
+                listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+                wplayer.URL = listBox1.Text;
+                label2.Text = wplayer.currentMedia.durationString;
+                label4.Text = wplayer.currentMedia.getItemInfo("Title");
+                wplayer.controls.play();
+                
+            }
+            catch 
+            {    
+                listBox1.SelectedIndex = -1;
+                listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+                wplayer.URL = listBox1.Text;
+                label2.Text = wplayer.currentMedia.durationString;
+            }       
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            wplayer.controls.play();
+            timer2.Enabled = false;
+        }
+    }
+}
